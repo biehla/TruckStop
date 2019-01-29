d5319b7cbf8cf9662c5290e977f373d16947c838
Elston <epeglab@gmail.com>
2019-01-29 11:17:55 -0500

    Revised Code Data

--- hack.py
+++ hack.py
@@ -20,99 +20,84 @@ lon = data1.lon.tolist()
 carID  = typ
 
 print("Total Values Found in  ", range(len(carID)))
-print(carID)
 
-latitude_car1[0] = latitude_car2[0] = latitude_car3[0] = latitude_car4[0] = latitude_car5[0] = latitude_car6[0] = latitude_car7[0] =  latitude_car8[0] =  latitude_care9[0] =  latitude_car10[0] = latitude_car11[0] = 0
 
-code_car1[0] = code_car2[0] = code_car3[0] = code_car4[0] = code_car5[0] = code_car6[0] = code_car7[0] = code_car8[0] = code_car9[0] = code_car10[0] = code_car11[0] = 0
+latitude_car1 = latitude_car2 = latitude_car3 = latitude_car4 = latitude_car5 = latitude_car6 = latitude_car7 =  latitude_car8 =  latitude_care9 =  latitude_car10 = latitude_car11 = []
+
+longitude_car1 = longitude_car2 = longitude_car3 = longitude_car4 = longitude_car5 = longitude_car6 = longitude_car7 =  longitude_car8 =  longitude_care9 =  longitude_car10 = longitude_car11 = []
+
+
+code_car1 = code_car2 = code_car3 = code_car4 = code_car5 = code_car6 = code_car7 = code_car8 = code_car9 = code_car10 = code_car11 = []
 
 print("Initializing Data...")
 
-a = b = c = d = e = f = g = h = g = k = l = m = n = o = 0
-
 for i in range(len(carID)):
     if carID[i] == "1088214034":
-        latitude_car1[a] = data[i,0]
-        longitude_car1[a]  = data[i,1]
-        code_car1[a] = data[i,4]
-        a = a + 1
-    if carID[i] == "1088214042":
-        latitude_car2[b] = data[i,0]
-        longitude_car2[b]  = data[i,1]
-        code_car2[b] = data[i,4]
-        b = b + 1
-    if carID[i] == "1088214013":
-        latitude_car3[c] = data[i,0]
-        longitude_car3[c]  = data[i,1]
-        code_car3[c] = data[i,4]
-        c += 1
-    if carID[i] == "1088217019":
-        latitude_car4[d] = data[i,0]
-        longitude_car4[d]  = data[i,1]
-        code_car4[d] = data[i,4]
-        d += 1
-    if carID[i] == "1088217018":
-        latitude_car5[e] = data[i,0]
-        longitude_car5[e]  = data[i,1]
-        code_car5[e] = data[i,4]
-        e += 1
-    if carID[i] == "1084068100":
-        latitude_car6[f] = data[i,0]
-        longitude_car6[f]  = data[i,1]
-        code_car6[f] = data[i,4]
-        f += 1
-    if carID[i] == "1084068111":
-        latitude_car7[g] = data[i,0]
-        longitude_car7[g]  = data[i,1]
-        code_car7[g] = data[i,4]
-        g += 1
-    if carID[i] == "1084067241":
-        latitude_car8[h] = data[i,0]
-        longitude_car8[h]  = data[i,1]
-        code_car8[h] = data[i,4]
-        print("AYY")
-        h += 1
-        
-    if carID[i] == "1088214007":
-        latitude_car9[j] = data[i,0]
-        longitude_car9[j]  = data[i,1]
-        code_car9[j] = data[i,4]
-        j += 1
-    if carID[i] == "1088214034":
-        latitude_car10[r] = data[i,0]
-        longitude_car10[r]  = data[i,1]
-        code_car10[r] = data[i,4]
-        r += 1
-    if carID[i] == "1088214236":
-        latitude_car11[p] = data[i,0]
-        longitude_car11[p]  = data[i,1]
-        code_car11[p] = data[i,4]
-        p += 1
+        latitude_car1.append(data[i,0])
+        longitude_car1.append(data[i,1])
+        code_car1.append(data[i,4])
+    elif carID[i] == "1088214042":
+        latitude_car2.append(data[i,0])
+        longitude_car2.append(data[i,1])
+        code_car2.append(data[i,4])
+    elif carID[i] == "1088214013":
+        latitude_car3.append(data[i,0])
+        longitude_car3.append(data[i,1])
+        code_car1.append(data[i,4])
+    elif carID[i] == "1088217019":
+        latitude_car4.append(data[i,0])
+        longitude_car4.append(data[i,1])
+        code_car4.append(data[i,4])
+    elif carID[i] == "1088217018":
+        latitude_car5.append(data[i,0])
+        longitude_car5.append(data[i,1])
+        code_car5.append(data[i,4])
+    elif carID[i] == "1084068100":
+        latitude_car6.append(data[i,0])
+        longitude_car6.append(data[i,1])
+        code_car6.append(data[i,4])
+    elif carID[i] == "1084068111":
+        latitude_car7.append(data[i,0])
+        longitude_car7.append(data[i,1])
+        code_car7.append(data[i,4])
+    elif carID[i] == "1084067241":
+        latitude_car8.append(data[i,0])
+        longitude_car8.append(data[i,1])
+        code_car8.append(data[i,4])
+    elif carID[i] == "1088214007":
+        latitude_car9.append(data[i,0])
+        longitude_car9.append(data[i,1])
+        code_car9.append(data[i,4])
+    elif carID[i] == "1088214034":
+        latitude_car10.append(data[i,0])
+        longitude_car10.append(data[i,1])
+        code_car10.append(data[i,4])
+    elif carID[i] == "1088214236":
+        latitude_car11.append(data[i,0])
+        longitude_car11.append(data[i,1])
+        code_car11.append(data[i,4])
 
 
 print("...Done")
 print( carID[1])
 
-#if():
-#    print("TRUE")
 
+# Make a map of all the data points on the globe
 
+fig = plt.figure(figsize=(20,20))
 
-# fig = plt.figure(figsize=(20,20))
+m = Basemap(projection = 'lcc', resolution = None, width = 8E6, height = 8E6, lat_0 = 45, lon_0=-100)
+m.etopo()
+#m.bluemarble()
 
-# m = Basemap(projection = 'lcc', resolution = None, width = 8E6, height = 8E6, lat_0 = 45, lon_0=-100)
-# m.etopo()
-# #m.bluemarble()
-
-# # MAP
-
-# for rows in range (len(longitude)-9000):
-#     x,y = m(longitude[rows], latitude[rows])
-#     print("plotting  x:" ,x, " y: ", y)
-#     plt.plot(x,y, 'ok', markersize=5)
+# MAP
+for rows in range (len(longitude_car8)):
+    x,y = m(longitude_car8[rows], latitude_car8[rows])
+    print("plotting  x:" ,x, " y: ", y)
+    plt.plot(x,y, 'ok', markersize=5)
     
 
-# matplotlib.use('TkAgg')
+matplotlib.use('TkAgg')
 
-# plt.show()
+plt.savefig('PlottedData.png')
 
